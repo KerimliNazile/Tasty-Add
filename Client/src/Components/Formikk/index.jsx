d6417f301c59fd05fd5c4e5e29cdefc318e03dbf
ismayilhsnv <tu7u02d2s@code.edu.az>
2024-01-22 14:11:10 +0400

    added

--- Client/src/Components/Formikk/index.jsx
+++ Client/src/Components/Formikk/index.jsx
@@ -1,55 +1,57 @@
-import React from 'react';
+import React from 'react'
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import axios from 'axios';
+import axios from 'axios'
+const FormAdd = ({getData}) => {
 
-const Formikk = ({ getData }) => {
-    async function AddMenu(values) {
-        const res = await axios.post("http://localhost:5000/deps", values)
+    async function addForm(values) {
+        const res=await axios.post("http://localhost:5000/deps",values)
         getData()
     }
-    return (
-        <Formik
-            initialValues={{ image: '', text: '', info: '', price: '' }}
-            validationSchema={Yup.object({
-                image: Yup.string()
-                    .max(150, 'Must be 150 characters or less')
-                    .required('Required'),
-                text: Yup.string()
-                    .max(20, 'Must be 20 characters or less')
-                    .required('Required'),
-                info: Yup.string()
-                    .max(20, 'Must be 20 characters or less')
-                    .required('Required'),
-                price: Yup.number()
-                    .max(20, 'Must be 20 characters or less')
-                    .required('Required'),
 
-            })}
-            onSubmit={(values, { resetForm }) => {
-                AddMenu(values)
-                resetForm()
-            }}
-        >
-            <Form>
-                <label htmlFor="text">Text</label>
-                <Field name="text" type="text" />
-                <ErrorMessage name="text" />
+  return (
+    <div>
+      <Formik
+      initialValues={{ image: '', text: '', info: '',price:'' }}
+      validationSchema={Yup.object({
+        image: Yup.string()
+          .max(15, 'Must be 15 characters or less')
+          .required('Required'),
+        text: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        info: Yup.string().required('Required'),
+        price: Yup.number().required('Required'),
+      })}
+      onSubmit={(values, { resetForm }) => {
+        addForm(values)
+        resetForm()
+      }}
+    >
+      <Form>
+        <label htmlFor="image">First Name</label>
+        <Field name="image" type="text" />
+        <ErrorMessage name="image" />
 
-                <label htmlFor="info">Info</label>
-                <Field name="info" type="text" />
-                <ErrorMessage name="info" />
+        <label htmlFor="text">Last Name</label>
+        <Field name="text" type="text" />
+        <ErrorMessage name="text" />
 
-                <label htmlFor="Image">Image</label>
-                <Field name="Image" type="text" />
-                <ErrorMessage name="Image" />
+        <label htmlFor="info">info Address</label>
+        <Field name="info" type="text" />
+        <ErrorMessage name="info" />
 
-                <label htmlFor="Price">Price</label>
-                <Field name="Price" type="number" />
-                <ErrorMessage name="Price" />
-                <button type="submit">Add</button>
-            </Form>
-        </Formik>
-    );
-};
-export default Formikk
+        <label htmlFor="price">price Address</label>
+        <Field name="price" type="price" />
+        <ErrorMessage name="price" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
+    </div>
+  )
+}
+
+export default FormAdd
+
+
+
--- Client/src/Pages/Addpage/index.jsx
+++ Client/src/Pages/Addpage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Formikk from '../../Components/Formikk'
+import FormAdd from '../../Components/Formikk'
 const Addpage = () => {
   const [data, setData] = useState([])
   const [search, setSearch] = useState('')
@@ -19,14 +20,14 @@ const Addpage = () => {
   return (
 
     <div>
-      <Formikk getData={getData} />
+      <FormAdd getData={getData} />
       <div className="filter">
         <input type="search" value={search} placeholder='search' onChange={(e) => setSearch(e.target.value)} />
-        <button onClick={() => setProperty({ text: "text", asc: null })}>default</button>
-        <button onClick={() => setProperty({ text: "text", asc: true })}>a-z</button>
-        <button onClick={() => setProperty({ text: "text", asc: false })}>z-a</button>
-        <button onClick={() => setProperty({ price: "price", asc: true })}>inc</button>
-        <button onClick={() => setProperty({ price: "price", asc: false })}>dec</button>
+        <button onClick={() => setProperty({ name: "text", asc: null })}>default</button>
+        <button onClick={() => setProperty({ name: "text", asc: true })}>a-z</button>
+        <button onClick={() => setProperty({ name: "text", asc: false })}>z-a</button>
+        <button onClick={() => setProperty({ name: "price", asc: true })}>inc</button>
+        <button onClick={() => setProperty({ name: "price", asc: false })}>dec</button>
       </div>
 
       <table>
@@ -45,9 +46,9 @@ const Addpage = () => {
             .filter(x => x.text.toLowerCase().includes(search.toLowerCase()))
             .sort((a, b) => {
               if (property && property.asc === true) {
-                return (a[property.text] < b[property.text]) ? -1 : ((b[property.text] < a[property.text]) ? 1 : 0)
+                return (a[property.name] < b[property.name]) ? -1 : ((b[property.name] < a[property.name]) ? 1 : 0)
               } else if (property && property.asc === false) {
-                return (a[property.text] > b[property.text]) ? -1 : ((b[property.text] > a[property.text]) ? 1 : 0)
+                return (a[property.name] > b[property.name]) ? -1 : ((b[property.name] > a[property.name]) ? 1 : 0)
               } else {
                 return 0;
               }
